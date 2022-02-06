80ddd6547ea423ef5c38b85d6c2ea65c0fdffded
Paulo Daniel Gonzalez <pdgonzalez872@gmail.com>
2022-02-06 16:43:50 -0600

    nice add more tests

--- test/html_parsers/usa_pickleball_parser_test.exs
+++ test/html_parsers/usa_pickleball_parser_test.exs
@@ -12,6 +12,29 @@ defmodule PickleTest do
       assert Enum.count(tournaments) == 117
 
       assert %{
+               address: "2343 E Tournament Way",
+               city: "Tucson",
+               end_date: end_date,
+               map_link:
+                 "https://maps.google.com/maps?f=q&source=s_q&hl=en&geocode=&q=2343+E+Tournament+Way+Tucson+AZ+85713+United+States",
+               name: "2022 Tucson Senior Olympic Festival",
+               organizer: "usa_pickleball",
+               prize_money: 0,
+               start_date: start_date,
+               state: "AZ",
+               url: "https://usapickleball.org/event/2022-tucson-senior-olympic-festival/",
+               zip: "85713"
+             } = Enum.at(tournaments, 0)
+
+      assert start_date.year == 2022
+      assert start_date.month == 1
+      assert start_date.day == 26
+
+      assert end_date.year == 2022
+      assert end_date.month == 1
+      assert end_date.day == 30
+
+      assert %{
                address: "77333 Country Club Dr",
                city: "Palm Desert",
                end_date: end_date,
