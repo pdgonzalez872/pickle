5019cf22e49d51fee6390c01e893474b384cb70c
Paulo Daniel Gonzalez <pdgonzalez872@gmail.com>
2022-02-06 21:25:51 -0600

    a lot closer

--- lib/pickle/workflows.ex
+++ lib/pickle/workflows.ex
@@ -1,5 +1,10 @@
 defmodule Pickle.Workflows do
-  @moduledoc false
+  @moduledoc """
+  These are "workflows". Actions we want to take and we want to be able to
+  reproduce.
+
+  This is a layer between the controllers and the contexts.
+  """
 
   require Logger
 
@@ -11,14 +16,16 @@ defmodule Pickle.Workflows do
        Pickle.UsaPickleballParser}
     ]
     |> Enum.each(fn {url, parser} ->
-      Task.start(scrape_tournaments(url, parser))
+      Task.start(fn ->
+        scrape_tournaments(url, parser)
+      end)
     end)
   end
 
   def scrape_tournaments(url, parser_mod) do
     with {:ok, html} <- tournament_scraper_impl().get_tournaments(url),
          tournaments <- parser_mod.call(html),
-         _result <- persist_tournaments(tournaments) do
+         {_successes, _} <- persist_tournaments(tournaments) do
       Logger.info("Successfully processed #{url}")
       {:ok, %{tournaments: tournaments, html: html}}
     else
@@ -36,9 +43,7 @@ defmodule Pickle.Workflows do
         |> Map.put(:updated_at, NaiveDateTime.truncate(NaiveDateTime.utc_now(), :second))
       end)
 
-    Pickle.Repo.insert_all(Pickle.Events.Tournament, tournaments_params_with_timestamps,
-      returning: true
-    )
+    Pickle.Repo.insert_all(Pickle.Events.Tournament, tournaments_params_with_timestamps)
   end
 
   defp tournament_scraper_impl() do
